34c3dc79feb03f173c1d1d8d9f96e3a1977e4ec2
Micaiah27 <113843952+Micaiah27@users.noreply.github.com>
2024-08-09 21:34:54 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import Explore from "./pages/Explore.page";
-import Events from "./pages/Events.page";
-import Places from "./pages/Places.page";
-import SignIn from "./pages/SignIn.page";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import HomePage from './pages/HomePage';
+import Explore from './pages/Explore.page';
+import Events from './pages/Events.page';
+import Places from './pages/Places.page';
+import SignInAndSignUpPage from './pages/SignInAndSignUp.page';
 // import PlaceDetails from './pages/PlaceDetails';
-// import Navbar from './services/Navbar';
-
+import Header from './services/Header';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -17,26 +16,13 @@ function App() {
 
   return (
     <Router>
-      {/*<Navbar user={user} />*/}
-      <Routes>
+      <Header user={user} />
+      <Routes> 
         <Route path="/" element={<HomePage />} />
-        <Route
-          exact
-          path="/explore"
-          element={<Explore places={places} setPlaces={setPlaces} />}
-        />
-        <Route
-          path="/events"
-          element={<Events events={events} setEvents={setEvents} />}
-        />
-        <Route
-          path="/places"
-          element={<Places places={places} setPlaces={setPlaces} />}
-        />
-        <Route
-          path="/signin"
-          element={<SignIn user={user} setUser={setUser} />}
-        />
+        <Route exact path="/explore" element={<Explore places={places} setPlaces={setPlaces} />} />
+        <Route path="/events" element={<Events events={events} setEvents={setEvents} />} />
+        <Route path="/places" element={<Places places={places} setPlaces={setPlaces} />} />
+        <Route path="/signin" element={<SignInAndSignUpPage user = {user} setUser = {setUser}/>} />
         {/* <Route path="/place/:id" element={<PlaceDetails places={places} />} /> */}
       </Routes>
     </Router>
